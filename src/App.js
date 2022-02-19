cd93c78bb16d4b11fd8eddf54ca15d9c599d0d69
NishantKumarTiwari <nishant@italentindia.com>
2022-02-18 21:34:10 -0800

    feat: added sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { useState } from 'react';
+
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import SideSearch from './components/searchSidebar/SideSearch';
-import Sidebar from './components/sideBar/Sidebar';
 
 function App() {
-  const [isSideSearch,setIsSideSearch] = useState(false)
-  const [isSidebar,setIsSidebar] = useState(false)
+
 
   return (
     <>
-    <Navbar sideSearchView={isSideSearch => setIsSideSearch(isSideSearch)} sideBarView={isSidebar => setIsSidebar(isSidebar)}/>
-    {isSideSearch?<SideSearch sideSearchView={isSideSearch => setIsSideSearch(isSideSearch)}/>:null}
-    {isSidebar?<Sidebar />:null}
+    <div className="main-cont">
+    <Navbar />
+
+    </div>
+    
+    
     </>
   );
 }
