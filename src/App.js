9abd92a52777828d102a90fb1ded187089c4a5e4
NishantKumarTiwari <nishant@italentindia.com>
2022-03-04 03:30:42 -0800

    feat:added register page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 
 import "./App.css";
-// import Login from "./components/log-reg/Login";
+import Login from "./components/log-reg/Login";
 import Register from "./components/log-reg/Register";
-// import Homepage from "./components/Homepage";
+import Homepage from "./components/Homepage";
 import Navbar from "./components/navbar/Navbar";
-// import ProductiListing from "./components/productListing/ProductiListing";
-// import SingleProductPage from "./components/singleProductPage/SingleProductPage";
+import ProductiListing from "./components/productListing/ProductiListing";
+import SingleProductPage from "./components/singleProductPage/SingleProductPage";
 
 function App() {
   return (
     <>
-      <div style={{height:'auto',width:'auto',overflow:'hidden'}}>
       <Navbar />
-      <div className="main-cont">
-      {/* <Homepage/> */}
-      {/* <ProductiListing/> */}
-      {/* <SingleProductPage/> */}
-      {/* <Login/> */}
-      <Register/>
-      </div>
+      <div style={{ height: 'auto', width: 'auto', overflow: 'hidden', position: 'relative' }}>
+        <div className="main-cont">
+          {/* <Homepage/> */}
+          {/* <ProductiListing /> */}
+          {/* <SingleProductPage/> */}
+          {/* <Login/> */}
+          <Register/>
+        </div>
       </div>
     </>
   );
--- src/components/log-reg/Register.js
+++ src/components/log-reg/Register.js
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import "./log-reg.css"
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+    <div className="log-reg-sec">
+        <form>
+            <div className="form-heading">
+            Register
+            </div>
+            <input type="text" className='form-username' placeholder='name'/>
+            <input type="text" className='form-username' placeholder='email address'/>
+            <input type="date" className='form-username' placeholder='Date of Birth'/>
+            <input type="password" className='form-username' placeholder='enter password'/>
+            <input type="password" className='form-username' placeholder='enter password again'/>
+            <div className="form-btn"><button>Let me in</button></div>
+            <div className="remember-me-cont">
+            <div className="remember-me">
+                or Login
+            </div>
+            <span>Privacy Policy</span>
+            </div>
+        </form>
+    </div>
+</>
   )
 }
 
