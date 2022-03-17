f46be2f23a9574c18db996d0855bcd269f70595e
Asuraking1n <nishant99tiwari@gmail.com>
2022-03-17 02:58:21 -0700

    extracted filter code

--- src/components/productListing/ProductiListing.js
+++ src/components/productListing/ProductiListing.js
@@ -4,44 +4,7 @@ import Insta from "../instagram/Insta";
 import Footer from "../footer/Footer";
 import "./productlisting.css";
 import { useProduct } from "../../context/product-context";
-
-const filterData = (arr, type) => {
-    switch (type) {
-        case 'SHOES':
-            return [...arr].filter((val) => val.categoryName === 'shoe')
-        case 'CLOTHING':
-            return [...arr].filter((val) => val.categoryName === 'men' || val.categoryName === 'women')
-        case 'ALL':
-            return [...arr]
-        case 'MEN':
-            return [...arr].filter((val) => val.categoryName === 'men')
-        case 'WOMEN':
-            return [...arr].filter((val) => val.categoryName === 'women')
-        case 'L':
-            return [...arr].filter((val) => val.size === 'L')
-        case 'M':
-            return [...arr].filter((val) => val.size === 'M')
-        case 'S':
-            return [...arr].filter((val) => val.size === 'S')
-        case 'US10':
-            return [...arr].filter((val) => val.size === 'US10')
-        case 'US9':
-            return [...arr].filter((val) => val.size === 'US9')
-        case 'US8':
-            return [...arr].filter((val) => val.size === 'US8')
-        case 'US7':
-            return [...arr].filter((val) => val.size === 'US7')
-        case 'US6':
-            return [...arr].filter((val) => val.size === 'US6')
-        case 'HIGH_TO_LOW':
-            return [...arr].sort((a, b) => b.price - a.price)
-        case 'LOW_TO_HIGH':
-            return [...arr].sort((a, b) => a.price - b.price)
-        default:
-            return [...arr]
-    }
-}
-
+import { FilterData } from "./FilterData";
 
 
 const ProductiListing = () => {
@@ -50,7 +13,7 @@ const ProductiListing = () => {
     const [state, dispatch] = useReducer(function filterReduce(state, action) {
         switch (action.type) {
             case 'SORT':
-                return { ...state, filterStore: filterData([...products], action.payload) }
+                return { ...state, filterStore: FilterData([...products], action.payload) }
             default:
                 return state
         }
