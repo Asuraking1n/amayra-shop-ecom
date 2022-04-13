8da57110c8a83f62e3e7ccc81c94c80620380abb
Asuraking1n <nishant99tiwari@gmail.com>
2022-04-13 01:24:11 -0700

    initial commit for dev

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -8,7 +8,7 @@ const CartContextProvider = ({ children }) => {
     const [cartState, cartDispatch] = useReducer(CartReducer, {cart:[] })
     return (
         <>
-            <cartContext.Provider value={{cartState, cartDispatch}}>
+            <cartContext.Provider value={{cartState, cartDispatch}}> 
                 {children}
             </cartContext.Provider>
         </>
